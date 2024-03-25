5a361670b67f0c1a4880e9fe290bb1ddfb9beb51
Sungjae.Kwak <sungjae8970@gmail.com>
2024-03-25 22:47:09 +0900

    chpater2 Test:e _ 미완성...

--- packages/chapter2/src/observer/pubsub.js
+++ packages/chapter2/src/observer/pubsub.js
@@ -1,7 +1,30 @@
-export const 구독 = fn => {
-  fn();
-}
+const state = {};
+let subscribers = [];
 
-export const 발행기관 = obj => {
-  return obj;
-}
+export const 구독 = (fn) => {
+  subscribers.push(fn.bind(state));
+  fn();
+};
+
+export const 발행기관 = (obj) => {
+  for (let [key, value] of Object.entries(obj)) {
+    // console.log(key, value);
+    Object.defineProperty(state, key, {
+      get() {
+        return value;
+      },
+      set(newVal) {
+        value = newVal;
+        subscribers.forEach((subscriber) => subscriber());
+      },
+      configurable: true,
+    });
+  }
+  return state;
+};
+
+const test = 발행기관({ a: 10, b: 1000 });
+console.log(test.a);
+console.log(test.b);
+test.a = 99;
+console.log(test.a);
